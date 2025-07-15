6cdcbe406e6fc2df9680ef60ca36664ed9080d69
Davi0A <codigododavi@gmail.com>
2025-07-15 09:32:56 -0300

    .

--- anna/javascript.js
+++ anna/javascript.js
@@ -2,29 +2,32 @@ const contquest = document.querySelector("#contquest")
 const startpad = document.querySelector("#startpad")
 const wrong = document.querySelector("#imgwrong")
 
-const linesQuest = ["linesq1", "linesq2", "linesq3", "linesq4", "linesq5"]
+const linesQuest = ["linesq1", "linesq2", "linesq3", "linesq4", "linesq5", "linesq6"]
 const lines = Object.fromEntries(linesQuest.map(line => [line, document.querySelectorAll(`.${line}`)]))
 
-const button = ["buttonq1", "buttonq2", "buttonq3", "buttonq4", "buttonq5"]
+const button = ["buttonq1", "buttonq2", "buttonq3", "buttonq4", "buttonq5", "buttonq6"]
 const buttonq = Object.fromEntries(button.map(button => [button, document.querySelectorAll(`.${button}`)]))
 
-const answersq = ["answers1", "answers2", "answers3", "answers4", "answers5"]
+const answersq = ["answers1", "answers2", "answers3", "answers4", "answers5", "answers6"]
 const answers = Object.fromEntries(answersq.map(answer => [answer, document.getElementById(`${answer}`)]))
 
-const answersq1 = ["answer11", "answer12", "answer13", "answer14", "answer15"]
+const answersq1 = ["answer11", "answer12", "answer13", "answer14"]
 const answers1 = Object.fromEntries(answersq1.map(answerq1 => [answerq1, document.getElementById(`${answerq1}`)]))
 
-const answersq2 = ["answer21", "answer22", "answer23", "answer24", "answer25"]
+const answersq2 = ["answer21", "answer22", "answer23", "answer24"]
 const answers2 = Object.fromEntries(answersq2.map(answerq2 => [answerq2, document.getElementById(`${answerq2}`)]))
 
-const answersq3 = ["answer31", "answer32", "answer33", "answer34", "answer35"]
+const answersq3 = ["answer31", "answer32", "answer33", "answer34"]
 const answers3 = Object.fromEntries(answersq3.map(answerq3 => [answerq3, document.getElementById(`${answerq3}`)]))
 
-const answersq4 = ["answer41", "answer42", "answer43", "answer44", "answer45"]
+const answersq4 = ["answer41", "answer42", "answer43", "answer44"]
 const answers4 = Object.fromEntries(answersq4.map(answerq4 => [answerq4, document.getElementById(`${answerq4}`)]))
 
-const answersq5 = ["answer41", "answer42", "answer43", "answer44", "answer45"]
+const answersq5 = ["answer51", "answer52", "answer53", "answer54"]
 const answers5 = Object.fromEntries(answersq5.map(answerq5 => [answerq5, document.getElementById(`${answerq5}`)]))
+
+const answersq6 = ["answer61", "answer62", "answer63", "answer64"]
+const answers6 = Object.fromEntries(answersq6.map(answerq6 => [answerq6, document.getElementById(`${answerq6}`)]))
 
 async function forQuest1() {
     return new Promise (function (resolve) {
@@ -149,12 +152,24 @@ async function forQuest5() {
     })
 }
 
+async function forQuest6() {
+    return configureQuestionTransition({
+        answerCorrect: answers5.answer52,
+        nextAnswers: answers.answers6,
+        currentButtons: buttonq.buttonq5,
+        nextButtons: buttonq.buttonq6,
+        nextLines: lines.linesq6,
+        currentLines: lines.linesq5
+    })
+}
+
 async function wait() {
     await forQuest1();
     await forQuest2();
     await forQuest3();
     await forQuest4();
     await forQuest5();
+    await forQuest6();
 }
 
 wait()
@@ -163,44 +178,61 @@ function answerWrong() {
     wrong.setAttribute("style", "animation: wrongAnimate 0.8s ease; animation-fill-mode: forwards;")
     setTimeout(() => {
         wrong.setAttribute("style", "animation: wrongAnimated 0.8s ease; animation-fill-mode: forwards;")
-    }, 1000)
+    }, 1600)
 }
 
 function clickWrong({
     answers,
     answersCorrect
 }) {
-    for (let i = 0; i < answers.length; i++) {
-        answers.forEach(answer => {if (answer !== answersCorrect) {
+    Object.values(answers).forEach(answer => {
+        if (answer !== answersCorrect) {
             answer.addEventListener("click", answerWrong)
-        }})
-    }
+        }
+    })
 }
 
 async function fixedQuest1() {
     return clickWrong({
         answers: answers1,
-        answersCorrect: answers1.answer13
+        answersCorrect: answer13
     })
 }
 
 async function fixedQuest2() {
     return clickWrong({
         answers: answers2,
-        answersCorrect: answers2.answer24
+        answersCorrect: answer24
     })
 }
 
 async function fixedQuest3() {
     return clickWrong({
         answers: answers3,
-        answersCorrect: answers3.answer31
+        answersCorrect: answer31
     })
 }
 
 async function fixedQuest4() {
     return clickWrong({
         answers: answers4,
-        answersCorrect: answers4.answer43
+        answersCorrect: answer43
     })
 }
+
+async function fixedQuest5() {
+    return clickWrong({
+        answers: answers5,
+        answersCorrect: answer52
+    })
+}
+
+async function clickedWrong() {
+    await fixedQuest1();
+    await fixedQuest2();
+    await fixedQuest3();
+    await fixedQuest4();
+    await fixedQuest5();
+}
+
+clickedWrong()
